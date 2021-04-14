cb6150bf02db53d71b2201599a2991cadf6a1a0a
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-04-14 11:49:19 -0300

    Initial configuration room

---
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -0,0 +1,15 @@
+package br.com.alura.agenda.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+import br.com.alura.agenda.model.Aluno;
+
+@Database(entities = {Aluno.class}, version = 1, exportSchema = false)
+public abstract class AgendaDatabase extends RoomDatabase {
+
+}
